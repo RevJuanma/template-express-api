0558b9abb99d8ecf73cc6ceb5aa8fae6c25c01b9
RevJuanma <88888570+RevJuanma@users.noreply.github.com>
2025-09-05 17:02:03 -0300

    delete logs

--- app/middlewares/authMiddleware.js
+++ app/middlewares/authMiddleware.js
@@ -6,16 +6,13 @@ export const authMiddleware = (req, res, next) => {
   if (!authHeader) return res.status(401).json({ error: "Token requerido" });
 
   const token = authHeader.split(" ")[1];
-  console.log("token", token);
   if (!token) return res.status(401).json({ error: "Token inválido" });
 
   try {
     const decoded = jwt.verify(token, SECRET);
-    console.log("decoded", decoded);
     req.user = decoded;
     next();
   } catch (err) {
-    console.log("ERR", err)
     return res.status(403).json({ error: "Token no válido o expirado" });
   }
 };
